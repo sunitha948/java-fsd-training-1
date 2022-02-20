df096ed8bd5c00f725fe048b021e78f40f74cae3
Parvathy940 <99461792+Parvathy940@users.noreply.github.com>
2022-02-20 23:06:15 +0530

    Add files via upload

--- java programs/SelectionSort.java
+++ java programs/SelectionSort.java
@@ -4,25 +4,23 @@ class SelectionSort
 	void sort(int arr[])
 	{  					
 		int n = arr.length; 
-		                                                    //{25,64,12,22,11} n=5
 
 		// One by one move boundary of unsorted subarray
-		for (int i = 0; i < n-1; i++)  
-		                                                      //n-1=4
+		for (int i = 0; i < n-1; i++) 
 		{
 			// Find the minimum element in unsorted array
-			int min_idx = i;                 //min=0
-			for (int j = i+1; j < n; j++)    //j=0+1=1 
+			int min_idx = i;
+			for (int j = i+1; j < n; j++) 
 				{
 				if (arr[j] < arr[min_idx])
-					min_idx = j;            //min=1
+					min_idx = j;
 			}
 
 			// Swap the found minimum element with the first
 			// element
-			int temp = arr[min_idx];  //temp=1
-			arr[min_idx] = arr[i];     //arr[min]=0
-			arr[i] = temp;            //arr[0]=1
+			int temp = arr[min_idx];
+			arr[min_idx] = arr[i];
+			arr[i] = temp;
 		}
 	}
 
